ca827bafa5c3d7b5c24e3ea59c6f403087631c5a
Maelys Tiffaney <maelysti@student.21-school.ru>
2022-12-13 21:05:58 +0300

    add sum sub and mult number tests

--- src/check.c
+++ src/check.c
@@ -1,9 +1,11 @@
 #include "tests/check_s21_service.h"
+#include "tests/check_s21_operations.h"
 
 int main() {
   int no_failed = 0;
 
   no_failed |= test_service();
+  no_failed |= test_operations();
 
   return (no_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
