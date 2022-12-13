3ea93f3f735d202b8bb220b985380552249eabc0
Maelys Tiffaney <maelysti@student.21-school.ru>
2022-12-13 21:33:38 +0300

    add tests for mult_matrix

--- src/tests/check_s21_operations.c
+++ src/tests/check_s21_operations.c
@@ -108,6 +108,48 @@ START_TEST(test_s21_mult_number) {
 }
 END_TEST
 
+START_TEST(test_s21_mult_matrix) {
+  matrix_t a;
+  matrix_t b;
+  matrix_t result;
+  matrix_t expected;
+  int return_code = 0;
+  s21_create_matrix(2, 2, &a);
+  s21_create_matrix(2, 2, &b);
+  s21_create_matrix(2, 2, &result);
+  s21_create_matrix(2, 2, &expected);
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      double temp = i * 2 + j + 1;
+      a.matrix[i][j] = temp;
+      b.matrix[i][j] = temp;
+    }
+  }
+  expected.matrix[0][0] = 7;
+  expected.matrix[0][1] = 10;
+  expected.matrix[1][0] = 15;
+  expected.matrix[1][1] = 22;
+  return_code = s21_mult_matrix(&a, &b, &result);
+  ck_assert_int_eq(return_code, 0);
+  return_code = s21_eq_matrix(&result, &expected);
+  ck_assert_int_eq(return_code, SUCCESS);
+  a.columns = 3;
+  return_code = s21_mult_matrix(&a, &b, &result);
+  ck_assert_int_eq(return_code, 2);
+  a.columns = 2;
+  return_code = s21_mult_matrix(NULL, &b, &result);
+  ck_assert_int_eq(return_code, 1);
+  return_code = s21_mult_matrix(&a, NULL, &result);
+  ck_assert_int_eq(return_code, 1);
+  return_code = s21_mult_matrix(&a, &b, NULL);
+  ck_assert_int_eq(return_code, 1);
+  s21_remove_matrix(&a);
+  s21_remove_matrix(&b);
+  s21_remove_matrix(&result);
+  s21_remove_matrix(&expected);
+}
+END_TEST
+
 
 Suite *s21_operations(void) {
   Suite *s;
@@ -120,6 +162,7 @@ Suite *s21_operations(void) {
   tcase_add_test(tc_core, test_s21_sub_matrix);
   tcase_add_test(tc_core, test_sum_and_sub);
   tcase_add_test(tc_core, test_s21_mult_number);
+  tcase_add_test(tc_core, test_s21_mult_matrix);
   suite_add_tcase(s, tc_core);
 
   return s;
