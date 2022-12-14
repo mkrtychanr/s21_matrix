3c55c887d80dbbbaadf11352f582f2860f16eea1
Maelys Tiffaney <maelysti@student.21-school.ru>
2022-12-14 03:24:37 +0300

    add tests for s21_inverse

--- src/tests/check_s21_linear_algebra.c
+++ src/tests/check_s21_linear_algebra.c
@@ -110,6 +110,65 @@ START_TEST(test_s21_calc_complements) {
 }
 END_TEST
 
+//      2  5  7
+// A =  6  3  4
+//      5 -2 -3
+
+
+START_TEST(test_s21_inverse_matrix) {
+    matrix_t a;
+    matrix_t result;
+    matrix_t expected;
+    int return_code = 0;
+    s21_create_matrix(3, 3, &a);
+    s21_create_matrix(3, 3, &expected);
+    a.matrix[0][0] = 2;
+    a.matrix[0][1] = 5;
+    a.matrix[0][2] = 7;
+    a.matrix[1][0] = 6;
+    a.matrix[1][1] = 3;
+    a.matrix[1][2] = 4;
+    a.matrix[2][0] = 5;
+    a.matrix[2][1] = -2;
+    a.matrix[2][2] = -3;
+    expected.matrix[0][0] = 1;
+    expected.matrix[0][1] = -1;
+    expected.matrix[0][2] = 1;
+    expected.matrix[1][0] = -38;
+    expected.matrix[1][1] = 41;
+    expected.matrix[1][2] = -34;
+    expected.matrix[2][0] = 27;
+    expected.matrix[2][1] = -29;
+    expected.matrix[2][2] = 24;
+    return_code = s21_inverse_matrix(&a, &result);
+    ck_assert_int_eq(return_code, 0);
+    return_code = s21_eq_matrix(&result, &expected);
+    ck_assert_int_eq(return_code, SUCCESS);
+    a.rows = 4;
+    return_code = s21_inverse_matrix(&a, &result);
+    ck_assert_int_eq(return_code, 2);
+    a.rows = 3;
+    return_code = s21_inverse_matrix(NULL, &result);
+    ck_assert_int_eq(return_code, 1);
+    return_code = s21_inverse_matrix(&a, NULL);
+    ck_assert_int_eq(return_code, 1);
+    s21_remove_matrix(&a);
+    s21_remove_matrix(&result);
+    s21_remove_matrix(&expected);
+    s21_create_matrix(3, 3, &a);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            a.matrix[i][j] = i * 3 + j + 1;
+        }
+    }
+    return_code = s21_inverse_matrix(&a, &result);
+    ck_assert_int_eq(return_code, 2);
+}
+
+//         -1   1  -1
+// М^T. =  38 -41  34
+//        -27  29 -24
+
 Suite *s21_linear_algebra(void) {
   Suite *s;
   TCase *tc_core;
@@ -120,6 +179,7 @@ Suite *s21_linear_algebra(void) {
   tcase_add_test(tc_core, test_s21_transpose_matrix);
   tcase_add_test(tc_core, test_s21_determinant);
   tcase_add_test(tc_core, test_s21_calc_complements);
+  tcase_add_test(tc_core, test_s21_inverse_matrix);
 
   suite_add_tcase(s, tc_core);
 
